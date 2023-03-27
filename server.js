918f18d07188ec7ad8a3cac28053ef942f1aed69
m07mmad-nasr <101505192+m07mmad-nasr@users.noreply.github.com>
2023-03-26 19:01:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http =require('http')
 const hostname = '0.0.0.0';
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const server =http.createServer((req,res)=>{
   res.statusCode =200;
